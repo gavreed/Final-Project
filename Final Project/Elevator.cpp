49ece8e4c9ba9831cbd12652af7d416413203990
ezhao894623 <ezhao894623@gmail.com>
2023-11-14 19:38:19 -0500

    Bug fixes, floor tick, elevator tick, and person read.

--- Final Project/Elevator.cpp
+++ Final Project/Elevator.cpp
@@ -19,8 +19,12 @@ void Elevator::tick(int currentTime) {
     if(currentTime % TICKS_PER_ELEVATOR_MOVE == 0 && servicing) {
         if(currentFloor - targetFloor > 0) {
             currentFloor--;
-        } else if(currentFloor - targetFloor < 0) {
+        }
+        else if(currentFloor - targetFloor < 0) {
             currentFloor++;
+        }
+        else if (currentFloor == targetFloor){
+            servicing = false;
         }
     }
 }
